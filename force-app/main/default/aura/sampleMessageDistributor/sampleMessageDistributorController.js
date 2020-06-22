b440c1c456997326d22538f59c4f58f146c20221
vsyniohub <storm6594@gmail.com>
2020-06-22 09:56:33 +0200

    Aura, LWC Messaging working

--- force-app/main/default/aura/sampleMessageDistributor/sampleMessageDistributorController.js
+++ force-app/main/default/aura/sampleMessageDistributor/sampleMessageDistributorController.js
@@ -1,10 +1,20 @@
 ({
     handlePublish: function(component, event, helper) {
         var payload = {
-            recordId: "ABC0000X1",
-            messageText: "Some Text Came Here",
+            recordId:           "ABC0000X1",
+            nameSent:           component.find("messageComponentName").get("v.value"),
+            messageText:        component.find("messageTextBox").get("v.value"),
             dateTimeGenerated : Date.now()
         };
         component.find("messageChannelComponent").publish(payload);
+    },
+    onMessage: function(component, message, helper) {
+        let messageThread = component.get("v.messageThread");
+        let name = message.getParam("nameSent");
+        let text = message.getParam("messageText");
+        component.set(
+            "v.messageThread", 
+            messageThread + name + ': ' + text + '\n'
+        );
     }
 })
--- force-app/main/default/lwc/sampleMessageDistributorWeb/sampleMessageDistributorWeb.js
+++ force-app/main/default/lwc/sampleMessageDistributorWeb/sampleMessageDistributorWeb.js
@@ -1,10 +1,13 @@
-import {wire, LightningElement } from 'lwc';
+import {track, wire, api, LightningElement } from 'lwc';
 import MESSAGE_CHANNEL from '@salesforce/messageChannel/DemoChannel__c';
-import {subscribe, publish, MessageContext } from 'lightning/messageService';
+import {subscribe, publish, APPLICATION_SCOPE, MessageContext } from 'lightning/messageService';
 
 export default class SampleMessageDistributorWeb extends LightningElement {
     messageReceived;
     subscription;
+    @track message = '';
+    @track messageThread = '';
+    @track name = 'Web Component';
 
     @wire(MessageContext)
     messageContext;
@@ -19,9 +22,29 @@ export default class SampleMessageDistributorWeb extends LightningElement {
             MESSAGE_CHANNEL, 
             (messageObject) => {
                 this.handleMessage(messageObject);
+            },
+            {
+                scope: APPLICATION_SCOPE 
             });
     }
     handleMessage(messageObject) {
         this.messageReceived = messageObject ? JSON.stringify(messageObject, null, '\t') : 'Empty';
+        this.messageThread = this.messageThread + messageObject.nameSent + ': ' + messageObject.messageText + '\n'
+    }
+    handlePublish() {
+        var payload = {
+            recordId:           "ABC0000X1",
+            nameSent:           this.name,
+            messageText:        this.message,
+            dateTimeGenerated : Date.now()
+        };
+        console.log(payload);
+        publish(this.messageContext, MESSAGE_CHANNEL, payload);
+    }
+    setMessage(event) {
+        this.message = event.target.value;
+    }
+    setName(event) {
+        this.name = event.target.value;
     }
 }
